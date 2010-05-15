f3d3b91c4a85ac6c199eec04544e46f6efa53b50
amercader <amercadero@gmail.com>
2010-05-15 14:43:55 +0200

    New folder structure

---
+++ php/class.i18n.php
@@ -0,0 +1,18 @@
+<?php
+	
+	class i18n {
+		
+		function __construct(){
+		}
+		
+		function setLang($lang = false){
+			if ($lang)
+			//Check value
+			//Check request parameter
+			//Check cookie
+			//Check user preferences
+			//Use default
+		}
+	}
+
+?>
