fb316d723c19571243502225d27d3623341f9c88
amercader <amercadero@gmail.com>
2010-05-17 18:04:31 +0200

    Add Piwik tracking code

--- www/pages/common.footer.php
+++ www/pages/common.footer.php
@@ -1,3 +1,15 @@
 	<div id="footer">
       &copy; 2010 badgelona
     </div>
+	<!-- Piwik -->
+	<script type="text/javascript">
+	var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.flentic.net/" : "http://stats.flentic.net/");
+	document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+	</script><script type="text/javascript">
+	try {
+	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+	piwikTracker.trackPageView();
+	piwikTracker.enableLinkTracking();
+	} catch( err ) {}
+	</script><noscript><p><img src="http://stats.flentic.net/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+	<!-- End Piwik Tag -->
